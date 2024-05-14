4c6fee12e63a11e0551c8b709405b012367f3ad5
fatmaaben <fatmabenrjab15@gmail.Com>
2024-05-14 14:42:01 +0100

    mailhog

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $email = (new Email())
+                ->from($data['email'])
+                ->to('recipient@example.com') // Remplacez par l'adresse email de destination
+                ->subject($data['subject'])
+                ->text($data['message']);
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a été envoyé avec succès !');
+
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'contactForm' => $form->createView(),
+        ]);
+    }
+}
