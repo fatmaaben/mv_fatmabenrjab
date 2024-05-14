90790c3231838cc0ed86485e503d6a8e5e17e952
fatmaaben <fatmabenrjab15@gmail.Com>
2024-05-14 13:29:14 +0100

    Pagination

--- src/Repository/VinylMixRepository.php
+++ src/Repository/VinylMixRepository.php
@@ -43,7 +43,7 @@ class VinylMixRepository extends ServiceEntityRepository
     /**
      * @return VinylMix[] Returns an array of VinylMix objects
      */
-    public function findAllOrderedByVotes(string $genre = null): array
+    public function createOrderedByVotesQueryBuilder(string $genre = null): QueryBuilder
     {
         $queryBuilder = $this->addOrderByVotesQueryBuilder();
 
@@ -52,10 +52,7 @@ class VinylMixRepository extends ServiceEntityRepository
                 ->setParameter('genre', $genre);
         }
 
-        return $queryBuilder
-            ->getQuery()
-            ->getResult()
-        ;
+        return $queryBuilder;
     }
 
     private function addOrderByVotesQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
